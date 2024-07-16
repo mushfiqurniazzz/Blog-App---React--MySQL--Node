e17093a716678080f57956a5df827081b81bb83e
Mushfiqur Niaz <mushfiqurniaz33@gmail.com>
2024-07-17 02:14:07 +0600

    Completed LoginPage componenet

--- client/src/pages/authpages/LoginPage.jsx
+++ client/src/pages/authpages/LoginPage.jsx
@@ -1,9 +1,137 @@
-import React from 'react'
+//importing important functions, libraries and style sheet to complete the login page functional componenet
+//use state for using use state variables in input fields
+import { useState } from "react";
+//axios for sending HTTP request
+import axios from "axios";
+//for sending toast notifications
+import { toast } from "sonner";
+//navigation and link function from dynamically go through pages without reloading the page
+import { useNavigate, Link } from "react-router-dom";
+import styles from "../../styles/LoginPage.module.css";
+import NavBar from "../../components/NavBar";
+import Footer from "../../components/Footer";
+//js-cookie library makes it easy to retrieve or set cokies from the frontend for the user
+import Cookies from "js-cookie";
 
+//the login page functional componenet which will first check if the user has a cookie called token in the cookie section else will continue to log the user in ans set a cookie
 const LoginPage = () => {
-  return (
-    <div>LoginPage</div>
-  )
-}
+  //check if the user has a cookie,dont let the user login once again
+  const CheckCookieExists = Cookies.get("token");
 
-export default LoginPage
+  //use nagivation varibale as cannot derictly use useNavigate in a function
+  const navigate = useNavigate();
+
+  //using state variablesfor accessing the values typed in the input fields for sign up
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  //function for sending a axios post request to the server using the credentials provided in the input fields
+  const LoginHandler = async (e) => {
+    //will prevent the default form submission behaviour that is reloading the page
+    e.preventDefault();
+
+    //check if all the fields are provided in the input field if all the fields are not provided send a toast warning notification stating all fields are required
+    if (!username || username === "" || !password || password === "") {
+      //returning a statement will end the function and will not continue
+      return toast.warning("All fields are required.");
+    }
+
+    //after all checks continue with the logging in the user by sending a post request to the server using axios with the provided credentials in a try catch block for better readability and maintainibility of our code
+    try {
+      const res = await axios.post(
+        "http://localhost:5000/auth/login",
+        {
+          username: username,
+          password: password
+        },
+        { withCredentials: true }
+      );
+
+      //if the sign up was successfull the server will return a 200 http res code then send a toast success notification, console log the user info like id, email and username of the user then finally navigate the user ot the / after logging in
+      if (res.status === 200) {
+        toast.success("Login successful, heading to home page.");
+        console.log(res.data);
+
+        setTimeout(() => {
+          navigate("/");
+        }, 3000);
+      }
+    } catch (error) {
+      //basic error handler incase of error
+      console.error(error);
+      toast.error("Something went wrong try again later.");
+    }
+  };
+  return (
+    <>
+    {/* react conditional rendering which will state some message if the cookie is present */}
+      {CheckCookieExists ? (
+        <div>
+          <NavBar />
+          <h1 className={styles.LoggedInUser}>
+            Cannot login when a user&apos;s already logged in.
+          </h1>
+          <Footer />
+        </div>
+      ) : (
+        <>
+          <NavBar />
+          <div className={styles.formparent}>
+            <form className={styles.form}>
+              <h3>Welcome Back.</h3>
+              <hr />
+              <div className="mb-3">
+                <label htmlFor="UsernameInput" className="form-label">
+                  Username
+                </label>
+                <input
+                  type="text"
+                  value={username}
+                  onChange={(e) => {
+                    setUsername(e.target.value);
+                  }}
+                  className="form-control"
+                  placeholder="Username"
+                  id="UsernameInput"
+                  aria-describedby="emailHelp"
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="PasswordInput" className="form-label">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  value={password}
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                  }}
+                  placeholder="Password"
+                  className="form-control"
+                  id="PasswordInput"
+                />
+              </div>
+              <div>
+                <p id={styles.formfooterp}>
+                  Don&apos;t have an account? <Link to="/signup">Signup</Link>
+                </p>
+                <button
+                  id={styles.formfooterbutton}
+                  type="submit"
+                  onClick={LoginHandler}
+                  className="btn btn-primary"
+                >
+                  Login
+                </button>
+              </div>
+            </form>
+          </div>
+          <Footer />
+        </>
+      )}
+    </>
+  );
+};
+
+//exporting the created function component sign up
+export default LoginPage;
