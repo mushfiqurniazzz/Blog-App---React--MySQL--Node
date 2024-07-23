d33525164da9252fd037229e0db6ec16adc86656
Mushfiqur Niaz <mushfiqurniaz33@gmail.com>
2024-07-24 00:38:23 +0600

    created 404 page in frontend

--- client/src/pages/FourOFourPage.jsx
+++ client/src/pages/FourOFourPage.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
+import styles from "../styles/FourOFourPage.module.css"
 
 const FourOFourPage = () => {
   return (
-    <div>FourOFourPage</div>
-  )
-}
+    <>
+      <NavBar />
+      <div className={styles.parentdiv}>
+        <h1>
+          This URL doesn&apos;t exist, click here to return to the home page.
+        </h1>
+        <Link id={styles.button} to={"/"} type="button" className="btn btn-primary">
+          Click Here
+        </Link>
+      </div>
 
-export default FourOFourPage
+      <Footer />
+    </>
+  );
+};
+
+export default FourOFourPage;
