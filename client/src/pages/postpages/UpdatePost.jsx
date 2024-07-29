99b1616dac9dd0951624a5dcca134bec9949f064
Mushfiqur Niaz <mushfiqurniaz33@gmail.com>
2024-07-30 02:41:06 +0600

    update post page completed in client

--- client/src/pages/postpages/UpdatePost.jsx
+++ client/src/pages/postpages/UpdatePost.jsx
@@ -1,5 +1,5 @@
 //importing neccessary functional components, libraries for completing the update post functional component use state and use ref for using state variables and having functionalities for rendering
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 //for sending HTTP reuests
 import axios from "axios";
 //for sending toast notifications to the user
@@ -10,7 +10,8 @@ import Navbar from "../../components/NavBar";
 import Footer from "../../components/Footer";
 //importing the same stylesheet as create post for re usability
 import styles from "../../styles/CreatePost.module.css";
-import { useNavigate } from "react-router-dom";
+//for navigating through the pages dinamicaly
+import { Link, useNavigate } from "react-router-dom";
 
 //update post functional componenet which first checks if the user is authenticated to update a post then continues to render input fields and send to server and if success sends a 200 success message
 const UpdatePost = () => {
@@ -23,11 +24,14 @@ const UpdatePost = () => {
     .pop()
     .replace(":", "");
 
-  //use state variable for dynamically changing and having hold of the value of post title input field
-  const [post_title, setPost_title] = useState("ok");
+  //use state variable for holding the use eddect data into which will be used for the uther state variables
+  const [PostData, setPostData] = useState([]);
 
   //use state variable for dynamically changing and having hold of the value of post title input field
-  const [post_body, setPost_body] = useState("ok");
+  const [post_title, setPost_title] = useState();
+
+  //use state variable for dynamically changing and having hold of the value of post body input field
+  const [post_body, setPost_body] = useState();
 
   //use ref variable for changing and having hold of the value of image input field
   const fileInputRef = useRef(null);
@@ -35,6 +39,35 @@ const UpdatePost = () => {
   //declaring the use navigate function for dynamically navigate to pages as cannot be directly used in a function
   const navigate = useNavigate();
 
+  //passing a function and then calling it in use effect which will be used to set the values of input fields for easy updating when the app gets mounted
+  useEffect(() => {
+    const FetchPostInfo = async () => {
+      //using try catch block for better readability of code
+      try {
+        const res = await axios.get(`http://localhost:5000/post/${id}`, {
+          withCredentials: true
+        });
+
+        //hold the data recieved from the http request in a variable
+        const data = res.data;
+
+        //if the server returns a success message indicating data has been recieved
+        if (res.status === 200) {
+          //set the state varuable with the data
+          setPostData(data);
+          setPost_title(PostData.post_title);
+          setPost_body(PostData.post_body);
+        }
+      } catch (error) {
+        //basic error handling incase of error
+        console.log(error);
+      }
+    };
+
+    FetchPostInfo();
+  }, [id]);
+
+  //async await function which will send a put request to the server and incase of success will render a success message and a few tasks and incase of error will render an error toast notification
   const UpdatePostHandler = async (e) => {
     //will prevent the default form submission behaviour that is reloading the page
     e.preventDefault();
@@ -75,24 +108,30 @@ const UpdatePost = () => {
         setPost_body("");
         fileInputRef.current.value = null;
 
+        //after 3sec navigate the user to the profile page
         setTimeout(() => {
-          navigate("/");
+          navigate("/userprofile");
         }, 3000);
       }
     } catch (error) {
       //basic error handler incase of error, console log the error mesage and send a toast message stating something went wrong
       console.error(error);
+      //render an error message incase of error
       toast.error("Something went wrong.");
     }
   };
+
   return (
     <>
       {!CheckCookieExists ? (
         <>
           <Navbar />
-          <h1 className={styles.cannotpost}>
-            Cannot create a blog post without authentication, login first.
-          </h1>
+          <div className={styles.cannotpost}>
+            <h1>
+              Cannot update a blog post without authentication, login first.
+            </h1>
+            <Link to={"/login"}>Click Here to Login</Link>
+          </div>
           <Footer />
         </>
       ) : (
@@ -166,4 +205,5 @@ const UpdatePost = () => {
   );
 };
 
+//exporting the created update post functional component
 export default UpdatePost;
